2685e438efa7894de432ba02520117c45e4f172a
ASUS <ASUS@LAPTOP-R8G8PKRL>
2025-03-14 20:37:38 +0530

    Added new web project from STS

---
+++ myCouponDistributionPrj/src/main/java/com/sales/MyCouponDistributionPrjApplication.java
@@ -0,0 +1,14 @@
+package com.sales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCouponDistributionPrjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCouponDistributionPrjApplication.class, args);
+		System.out.println("Hi Neha");
+	}
+
+}
